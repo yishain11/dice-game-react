37e5549be8f2518169165cef78b6827e4fa4d9db
yishain11 <yishain11@walla.co.il>
2024-11-12 13:09:51 +0200

    added 2 dice rolls, add value to player score

--- dice-game/src/App.jsx
+++ dice-game/src/App.jsx
@@ -6,9 +6,21 @@ function App() {
     currentScore: 0
   };
   const targetScore = 50;
+
+  function throwDice() {
+    const num1 = Math.floor(Math.random() * 6 + 1);
+    const num2 = Math.floor(Math.random() * 6 + 1);
+    return [num1, num2];
+  }
+
+  function handleDiceThrow() {
+    const [num1, num2] = throwDice();
+    player.currentScore += (num1 + num2);
+  }
+
   return (
     <>
-
+      <button onClick={handleDiceThrow}>throw dice</button>
     </>
   )
 }
